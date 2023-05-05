e55c988fe663ac9564cb97bbdb7f9adc7060a9d6
Ben Boby <47517645+benbobyabraham@users.noreply.github.com>
2023-05-05 12:25:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div style={{display:'flex'}}>
-          <a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/ben-boby-3ba0a8144?trk=profile-badge" target="_blank">
+          <a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/ben-boby-3ba0a8144?trk=profile-badge" target="_blank" rel="noreferrer">
             <img src={logo} className="App-logo" alt="logo" />
           </a>
           <LinkedinBadge/>
